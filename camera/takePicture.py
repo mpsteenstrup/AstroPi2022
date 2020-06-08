4a404d72d68ada335e5f7a10f584f8b47dfe4dc2
mpsteenstrup <mpsteenstrup@gmail.com>
2020-06-08 10:08:39 +0200

    close camera automatic

--- camera/takePicture.py
+++ camera/takePicture.py
@@ -4,7 +4,15 @@ from time import sleep
 camera = PiCamera()
 
 # tager fire billeder og navngiver dem img0.jpg osv.
-for i in range(4):
+#for i in range(4):
    # giv tid til at kameraet kan indstille sig
-   sleep(2)
-   camera.capture('img%d.jpg'%i)
+#   sleep(2)
+#   camera.capture('img%d.jpg'%i)
+
+
+
+
+with PiCamera() as camera:
+    for i in range(4):
+        sleep(2)
+        camera.capture('img%d.jpg'%i)
