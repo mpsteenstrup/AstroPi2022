7340852640e0c434531633718680ed90b3ecba33
Mads Peter Steenstrup <mpsteenstrup@gmail.com>
2020-06-08 09:54:48 +0200

    hej

--- camera/diffPi.py
+++ camera/diffPi.py
@@ -10,13 +10,17 @@ import numpy as np
 
 
 with PiCamera() as camera:
-    rawCapture = PiRGBArray(camera)    # giver kameraet tid til at indstille sig
+    rawCapture = PiRGBArray(camera)    
+    # giver kameraet tid til at indstille sig
     sleep(1)
     camera.capture(rawCapture, format='bgr') # bemærk at vi gemmer som blå, grøn, rød
     frame1 = rawCapture.array
 
-frame2 = frame1
-
+    rawCapture = PiRGBArray(camera)    
+    # giver kameraet tid til at indstille sig
+    sleep(1)
+    camera.capture(rawCapture, format='bgr') # bemærk at vi gemmer som blå, grøn, rød
+    frame2 = rawCapture.array
 
 # convert to grayscale
 frame1 = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
@@ -30,7 +34,6 @@ frame2 = cv2.GaussianBlur(frame2, (21, 21), 10)
 delta_frame=cv2.absdiff(frame1,frame2)
 
 while True:
-
     frame=np.concatenate((frame1,frame2,delta_frame),axis=1)
     frame = cv2.resize(frame, (1160, 440))
     cv2.imshow('side by side',frame)
@@ -42,8 +45,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
         print('done')
-
-
 
 
 # Destroy all the windows
