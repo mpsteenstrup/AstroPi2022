d569e89bef50efa0cf59cac13bd78e96abbbfdaf
Mads Peter Steenstrup <mpsteenstrup@gmail.com>
2020-06-08 10:11:52 +0200

    rettet

--- camera/takePicture.py
+++ camera/takePicture.py
@@ -1,17 +1,7 @@
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera()
-
-# tager fire billeder og navngiver dem img0.jpg osv.
-#for i in range(4):
-   # giv tid til at kameraet kan indstille sig
-#   sleep(2)
-#   camera.capture('img%d.jpg'%i)
-
-
-
-
+# åbner PiCamera som camera. På denne måde lukker det rigtigt igen når man er færdig
 with PiCamera() as camera:
     for i in range(4):
         sleep(2)
