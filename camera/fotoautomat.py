f8df6023c1317d23733e785c0a89d12a154c1287
mpsteenstrup <mpsteenstrup@gmail.com>
2020-06-08 10:17:23 +0200

    fotoautomat

---
+++ camera/fotoautomat.py
@@ -0,0 +1,14 @@
+from picamera import PiCamera
+from time import sleep
+
+# åbner PiCamera som camera. På denne måde lukker det rigtigt igen når man er færdig
+with PiCamera() as camera:
+    for i in range(4):
+        print(3)
+        sleep(1)
+        print(2)
+        sleep(1)
+        print(1)
+        sleep(1)
+        print('GO')
+        camera.capture('img%d.jpg'%i)
