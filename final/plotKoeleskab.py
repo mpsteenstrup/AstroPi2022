454057cca2d56e2e7739e5c6cbb2f7d10d87b1d2
mpsteenstrup <mpsteenstrup@gmail.com>
2020-07-08 09:19:00 +0200

    plot

---
+++ final/plotKoeleskab.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import csv
+
+x = []
+t = []
+p = []
+
+with open('koeleskab.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))
+        t.append(int(row[1]))
+        p.append(int(row[]2))
+
+plt.plot(x,t, label='Loaded from file!')
+plt.xlabel('tid')
+plt.ylabel('temperatur')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()
