f8c7d5e9989a10be8dfe58a6a2659087a1418982
mpsteenstrup <mpsteenstrup@gmail.com>
2020-11-22 09:38:33 +0100

    real directory

--- pythonFiler/gemDataKolonner.py
+++ pythonFiler/gemDataKolonner.py
@@ -1,14 +1,17 @@
 import datetime
 from time import sleep
+import os
 from sense_hat import SenseHat
 sense = SenseHat()
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+filename = dir_path + '/test.csv'
 
 start_time = datetime.datetime.now()
 now_time = datetime.datetime.now()
 duration = datetime.timedelta(seconds=30)
 
-with open ("test.csv", "w") as file:
+with open ("filename", "w") as file:
     file.write("time , Temperature , pressure \n")
 
 while now_time < start_time + duration:
@@ -16,6 +19,6 @@ while now_time < start_time + duration:
     p = sense.get_pressure()
     now_time= datetime.datetime.now()
 
-    with open ("test.csv", "a") as file:
+    with open ("filename", "a") as file:
         file.write("%s, %s, %s  \n" % (now_time, t,p))
     sleep(1)
